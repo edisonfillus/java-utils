143620ba477b2325209450d13ed78ec3cdec1280
Edison Klafke Fillus <edison.fillus@gmail.com>
2021-01-25 21:12:46 -0300

    include multiplication table

--- src/main/java/com/company/FormatStringConsole.java
+++ src/main/java/com/company/FormatStringConsole.java
@@ -3,6 +3,17 @@ package com.company;
 public class FormatStringConsole {
 
     public static void main(String ...args){
+        printReport();
+        printMultiplicationTable(2);
+    }
+
+    public static void printMultiplicationTable(int n){
+        for(int i = 1 ; i <= 10 ; i++){
+            System.out.println(String.format("%d x %d = %d",n,i,n*i));
+        }
+    }
+
+    public static void printReport(){
         String[] categories = new String[]{"java", "python", "c++"};
         int[] values = new int[]{100,56,78};
 
@@ -13,7 +24,6 @@ public class FormatStringConsole {
             System.out.println("");
         }
         System.out.println("================================");
-
     }
 
 }
